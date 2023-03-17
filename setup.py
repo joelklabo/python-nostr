d82a9b13bde8695173c84fbb9c060e0ce2b1c3ed
Joel Klabo <joelklabo@gmail.com>
2023-03-17 13:15:42 -0700

    Adding setup.py info

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='nostr',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'cffi>=1.15.0',
+        'cryptography>=37.0.4',
+        'pycparser>=2.21',
+        'secp256k1>=0.14.0',
+        'websocket-client>=1.3.3',
+    ]
+)
